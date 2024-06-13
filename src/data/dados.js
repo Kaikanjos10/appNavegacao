dc2b307a98e825e8ce634e73e0578f93d618f1e9
kaik.anjos <anjoskaik58@gmail.com>
2024-06-13 13:33:31 -0300

    adiciona uma listagem de produtos

---
+++ src/data/dados.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: "1",
+        nome: "Notebook Dell Inspiron X",
+        preco: "4399,00",
+        fabricante: "Dell",
+        cor: "Preta",
+        linkImagem: "https://cdn.pixabay.com/photo/2016/08/28/17/30/laptop-1626471_1280.png"
+    },
+    {
+        id: "2",
+        nome: "Notebook Dell Inspiron X",
+        preco: "4399,00",
+        fabricante: "Dell",
+        cor: "Preta",
+        linkImagem: "https://cdn.pixabay.com/photo/2016/08/28/17/30/laptop-1626471_1280.png"
+    },
+    {
+        id: "3",
+        nome: "Notebook Dell Inspiron X",
+        preco: "4399,00",
+        fabricante: "Dell",
+        cor: "Preta",
+        linkImagem: "https://cdn.pixabay.com/photo/2016/08/28/17/30/laptop-1626471_1280.png"
+    }
+]
